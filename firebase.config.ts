6e0fa70ce6c2a94f2d85a2d74f7661cbeb197622
nataliia-v <nataly.verbenskaya@gmail.com>
2020-09-22 07:51:26 +0300

    MN-2-integration-with-firebase: + added firebase coonfig

---
+++ firebase.config.ts
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDPbrXhCFF2cetsGl9_xHZTuBRLe99nh0Y",
+  authDomain: "my-notes-c73de.firebaseapp.com",
+  databaseURL: "https://my-notes-c73de.firebaseio.com",
+  projectId: "my-notes-c73de",
+  storageBucket: "my-notes-c73de.appspot.com",
+  messagingSenderId: "901151126511",
+  appId: "1:901151126511:web:f39ba1435b532d158ffc1a"
+}
