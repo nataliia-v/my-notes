631bb18135b1f7f736c256a15b1a132ab4028b77
nataliia-v <nataly.verbenskaya@gmail.com>
2020-12-10 11:33:25 +0200

    MN-14-base-yser-authorization: + signIn, logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-// import * as firebase from 'firebase';
+import React, {useEffect, useRef, useState} from 'react';
+import firebase from 'firebase';
+
+import { auth, signInWithGoogle } from './firebase';
+
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState();
+  const unsubscribeFromAuth = useRef<firebase.Unsubscribe>();
+  
+  useEffect(() => {
+    unsubscribeFromAuth.current = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user)
+    });
+    return () => {
+      if (unsubscribeFromAuth.current) {
+        unsubscribeFromAuth.current();
+      }
+    }
+  }, [])
+  
+  
   return (
-    <div className="App" data-test='component'>
-     <p data-test='component'>
-       text
-     </p>
+    <div>
+      {currentUser ? (
+        <div>
+          <div>
+            <img src={currentUser.photoURL} />
+          </div>
+          <div>Name: {currentUser.displayName}</div>
+          <div>Email: {currentUser.email}</div>
+
+          <button onClick={() => auth.signOut()}>LOG OUT</button>
+        </div>
+        ) : <button onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</button>}
     </div>
   );
 }
